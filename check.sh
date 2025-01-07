dacbb0e3a0fd00180eac1070457a82c5734e25ca
p6nj <breee@duck.com>
2025-01-07 14:43:45 -0500

    don't have tests so don't check that

--- check.sh
+++ check.sh
@@ -6,6 +6,4 @@ cargo check --quiet --workspace --all-targets
 cargo check --quiet --workspace --all-features --lib --target wasm32-unknown-unknown
 cargo fmt --all -- --check
 cargo clippy --quiet --workspace --all-targets --all-features --  -D warnings -W clippy::all
-cargo test --quiet --workspace --all-targets --all-features
-cargo test --quiet --workspace --doc
 trunk build
